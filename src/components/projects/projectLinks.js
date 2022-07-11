a4add6408a65a77470b869d7b9676ca669cc7792
Alkibijad <aleksandar04081@gmail.com>
2022-07-11 15:01:45 -0700

    nike background image added

--- src/components/projects/projectLinks.js
+++ src/components/projects/projectLinks.js
@@ -2,7 +2,7 @@ let projectsLink = [
   {
     id: 1,
     siteName: "Nike",
-    backgroundImage: "https://i.pinimg.com/originals/d6/dc/00/d6dc008ab978b02f777ab8545eefeac5.jpg",
+    backgroundImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/1200px-Logo_NIKE.svg.png",
     project: "https://0408nike.netlify.app/",
     about:
       "I chose nike.com site, to demonstrate ability to where I’ll be working based on the task given, and recreate the best possible copy of existing project. For this page I have created JSON on my GitHub, then using AJAX to fetch data. Styling is done using SCSS.",
