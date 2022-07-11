6588de9fbd2dc3c77cc65643788a2e8ccbac24da
Alkibijad <aleksandar04081@gmail.com>
2022-07-11 14:13:54 -0700

    project work in progress page

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
-import './projects.css'
+import { useState, useEffect } from 'react'
+import projectLinks from "./projectLinks"
 
 function Projects() {
+
+  const [links, setLinks] = useState();
+
+  useEffect(() => { 
+    console.log(links);
+    setLinks(projectLinks)
+  })
+
+
+
+
+
+
+  const createProjectCard = () => { 
+    return <>{links.map((item) => { 
+      return <div key={ item.id} className='card_container'>
+        <button><a href={item.project}>Visit Nike</a></button>
+        <p className='card_text'>{item.about }</p>
+        </div>
+    })}</>
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div>Projects</div>
+    <div> {links ?  createProjectCard() : null}</div>
   )
 }
 
---
+++ src/components/projects/projectLinks.js
@@ -0,0 +1,27 @@
+let projectsLink = [
+  {
+    id: 1,
+    project: "https://app.netlify.com/sites/0408nike/overview",
+    about:
+      "I chose nike.com site, to demonstrate ability to where I’ll be working based on the task given, and recreate the best possible copy of existing project. For this page I have created JSON on my GitHub, then using AJAX to fetch data. Styling is done using SCSS.",
+  },
+  {
+    id: 2, 
+    project: "https://app.netlify.com/sites/0408furnitureshop/overview",
+    about:
+      "Furniture store page a task to demonstrate the ability to create pixel exact and responsive pages.For styling I’ve used CSS.",
+  },
+  {
+    id: 3, 
+    project: "https://0408cofferoasters.netlify.app/",
+    about:
+      "Cofeeroasters page is created using Vanilla JS. Demonstrate the skills of creating single page application with JS. I have used Figma design.",
+  },
+  { id: 4,
+    project: "https://0408travelspace.netlify.app/",
+    about:
+      "Space travel app is another JS app, where I have used AJAX and pure JS to create single page App. Styling is done in SCC based on Figma design.",
+  },
+];
+
+export default projectsLink;
